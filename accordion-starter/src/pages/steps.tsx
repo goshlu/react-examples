848da4fce73ecfcb8178db293eb0d698def70e46
yskm <yangshiheclow@gamil.com>
2024-06-10 18:18:27 +0800

    完善手风琴

---
+++ accordion-starter/src/pages/steps.tsx
@@ -0,0 +1,86 @@
+import {useState} from "react";
+
+const messages = [
+    'Step 1: Select the bread type',
+    'Step 2: Select the bread type',
+    'Step 3: Add your desired toppings',
+]
+
+function Steps () {
+    const [step, setStep] = useState(1)
+    const [isOpen, setIsOpen] = useState(false)
+
+    function handlePrevious() {
+        if(step > 1) {
+            setStep((s) => s - 1)
+        }
+    }
+
+    function handleNext() {
+        if(step < 3) {
+            setStep((s) => s + 1)
+        }
+    }
+
+    return (
+
+        <div className="steps">
+            <button className="close" onClick={() => setIsOpen((is) => !is)}>
+                &times;
+            </button>
+
+            {
+                isOpen && (
+                    <div className="steps">
+                        <div className="numbers">
+                            <div className={step >= 1 ? "active" : ""}>1</div>
+                            <div className={step >= 2 ? "active" : ""}>2</div>
+                            <div className={step >= 3 ? "active" : ""}>3</div>
+                        </div>
+
+                        {/*<p className="message">*/}
+                        {/*    Step {step} {messages[step - 1]}*/}
+                        {/*    /!*    {test.name}*!/*/}
+                        {/*</p>*/}
+                        <StepMessage step={step}>{
+                            messages[step - 1]
+                        }</StepMessage>
+
+                        <div className="buttons">
+                            <Button bgColor="#333" textColor="#fff" onClick={handlePrevious}>
+                                <span>👈</span> Previous
+                            </Button>
+                            <Button bgColor="7950f2" textColor="#fff" onClick={handleNext}>
+                                Next <span>👉</span>
+                            </Button>
+                        </div>
+                    </div>
+                )
+            }
+
+        </div>
+    )
+}
+
+function StepMessage({ step, children}) {
+    return (
+        <div className="message">
+            <h3>Step {step}</h3>
+            {children}
+        </div>
+    )
+}
+
+function Button({textColor, bgColor, onClick, children}) {
+    return (
+        <button
+            style={{backgroundColor: bgColor, color: textColor}}
+            onClick={onClick}
+        >
+            {children}
+        </button>
+
+    )
+}
+
+export default Steps;
